8c578fc6531593b41de58782731a822305fb58d8
Guilherme Fernandes <aateg@live.com>
2019-06-27 20:42:48 -0300

    proof of work

--- blockchain.py
+++ blockchain.py
@@ -55,6 +55,39 @@ class Blockchain(object):
         })
         return self.last_block['index'] + 1
 
+    def proof_of_work(self, last_proof):
+        """
+            Simple Proof of Work Algorithm:
+                - Find a number p' such that hash(pp') contains leading
+                4 zeroes, where p is the previous p'
+                - p is the previous proof, and p' is the new proof
+            Arguments:
+                - last_proof: <int>
+            Return:
+                - <int>
+        """
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+            Validates the Proof: does hash(last_proof, proof)
+            contain 4 leading zeroes?
+
+            Arguments:
+                - last_proof: <int> Previous Proof
+                - proof: <int> Current Proof
+            Return:
+                - <bool> True if correct, Fals if not
+        """
+        guess = f'{last_proof}{proof}'.encode() # converts to unicode
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == '0000'
+
+
     @staticmethod
     def hash(block):
         """
